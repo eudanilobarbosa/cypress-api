bb458f6227fc95982e9210d0c5ef8674fccd1496
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-04-28 14:25:33 -0300

    Seção 3: Testes e2e para APIs > 16. POST Request

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,5 +1,26 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+
+describe('POST /users', () => {
+
+
+  it('register a new user', () => {
+
+    const user = {
+      name: 'Fernando Papito',
+      email: 'papito@yahoo.com',
+      password: 'pwd123'
+    }
+
+    cy.request({
+      url: '/users',
+      method: 'POST',
+      body: user,
+      failOnStatusCode: false
+    }).then(response => {
+      expect(response.status).to.eq(200)
+    })
+
   })
+
+
 })
