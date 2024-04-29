18a7310131cfd190fd6881320e30bcfa405ad719
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-04-29 10:51:10 -0300

    Seção 3: Testes e2e para APIs > 21. Ganchos e campos obrigatórios

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -36,6 +36,57 @@ describe('POST /users', () => {
         expect(message).to.eq('Duplicated email!')
       })
   })
+
+  context('required fields', () => {
+    let user;
+
+    beforeEach(() => {
+      user = {
+        name: 'Margot Robbie',
+        email: 'margot@gmail.com',
+        password: 'pwd123'
+      }
+    })
+
+    it('name is required', () => {
+      delete user.name
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"name\" is required')
+        })
+    })
+
+    it('email is required', () => {
+      delete user.email
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"email\" is required')
+        })
+    })
+
+    it('password is required', () => {
+      delete user.password
+
+      cy.postUser(user)
+        .then(response => {
+
+          const { message } = response.body
+
+          expect(response.status).to.eq(400)
+          expect(message).to.eq('ValidationError: \"password\" is required')
+        })
+    })
+  })
 })
 
 
