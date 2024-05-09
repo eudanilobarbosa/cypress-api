ee4c34cd76676fb374708cddf2fe8a92769169b6
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-05-09 10:23:27 -0300

    Seção 4: Avançando nos testes > 26. Adicionando o token no header

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,7 +1,35 @@
 describe('POST /tasks', () => {
 
+    beforeEach(function () {
+        cy.fixture('tasks').then(function (tasks) {
+            this.tasks = tasks
+        })
+    })
 
     it('register a new task', function () {
+
+        const { user, task } = this.tasks.create
+
+        cy.postSession(user)
+            .then(response => {
+                cy.log(response.body.token)
+
+                cy.api({
+                    url: '/tasks',
+                    method: 'POST',
+                    body: task,
+                    headers: {
+                        authorization: response.body.token
+                    },
+                    failOnStatusCode: false
+                }).then(response => {
+                    expect(response.status).to.eq(200)
+                })
+
+
+            })
+
+
 
     })
 
