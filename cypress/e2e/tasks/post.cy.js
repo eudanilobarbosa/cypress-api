127ca00622a73ff73ce1ac0c3f4fb5ca30c2c79b
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-06-03 15:24:02 -0300

    Seção 5: Testando mensageria > 39. Validando fila de mensagens

--- cypress/e2e/tasks/post.cy.js
+++ cypress/e2e/tasks/post.cy.js
@@ -1,31 +1,52 @@
 describe('POST /tasks', () => {
-
     beforeEach(function () {
         cy.fixture('tasks/post').then(function (tasks) {
             this.tasks = tasks
         })
     })
 
-    it('register a new task', function () {
-        const { user, task } = this.tasks.create
+    context.only('register a new task', function () {
+        before(function () {
+            cy.purgeQueueMessages()
+                .then(response => {
+                    expect(response.status).to.eq(204)
+                })
+        })
 
-        cy.task('removeUser', user.email)
-        cy.postUser(user)
+        it('post task', function () {
+            const { user, task } = this.tasks.create
 
-        cy.postSession(user)
-            .then(userResp => {
-                cy.task('removeTask', task.name, user.email)
+            cy.task('removeUser', user.email)
+            cy.postUser(user)
 
-                cy.postTask(task, userResp.body.token)
-                    .then(response => {
-                        expect(response.status).to.eq(201)
-                        expect(response.body.name).to.eq(task.name)
-                        expect(response.body.tags).to.eql(task.tags)
-                        expect(response.body.is_done).to.be.false
-                        expect(response.body.user).to.eq(userResp.body.user._id)
-                        expect(response.body._id.length).to.eq(24)
-                    })
-            })
+            cy.postSession(user)
+                .then(userResp => {
+                    cy.task('removeTask', task.name, user.email)
+
+                    cy.postTask(task, userResp.body.token)
+                        .then(response => {
+                            expect(response.status).to.eq(201)
+                            expect(response.body.name).to.eq(task.name)
+                            expect(response.body.tags).to.eql(task.tags)
+                            expect(response.body.is_done).to.be.false
+                            expect(response.body.user).to.eq(userResp.body.user._id)
+                            expect(response.body._id.length).to.eq(24)
+                        })
+                })
+        })
+
+        after(function () {
+            const { user, task } = this.tasks.create
+            // get message
+            cy.wait(3000) // thinking time
+            cy.getMessageQueue()
+                .then(response => {
+                    expect(response.status).to.eq(200)
+                    expect(response.body[0].payload).to.include(user.name.split(' ')[0])
+                    expect(response.body[0].payload).to.include(task.name)
+                    expect(response.body[0].payload).to.include(user.email)
+                })
+        })
     })
 
     it('duplicate task', function () {
@@ -46,5 +67,4 @@ describe('POST /tasks', () => {
                     })
             })
     })
-
 })
