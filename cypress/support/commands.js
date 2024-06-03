f4fc706ab4e84a3901ceeb09dda5d2152535c156
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-06-03 14:05:35 -0300

    Seção 4: Avançando nos testes > 37. PUT Request

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -88,3 +88,14 @@ Cypress.Commands.add('deleteTask', (taskId, token) => {
         failOnStatusCode: false
     }).then(response => { return response })
 })
+
+Cypress.Commands.add('putTaskDone', (taskId, token) => {
+    cy.api({
+        url: `/tasks/${taskId}/done`,
+        method: 'PUT',
+        headers: {
+            authorization: token
+        },
+        failOnStatusCode: false
+    }).then(response => { return response })
+})
