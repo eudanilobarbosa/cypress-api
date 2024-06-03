a2a5c955e5a667d310d34aa401effdcaf42263e1
Danilo <eudanilobarbosa@users.noreply.github.com>
2024-06-03 09:35:04 -0300

    Seção 4: Avançando nos testes > 33. Testando GET de forma inusitada

--- cypress/e2e/tasks/get.cy.js
+++ cypress/e2e/tasks/get.cy.js
@@ -1,8 +1,39 @@
 describe('GET /tasks', () => {
 
+    beforeEach(function () {
+        cy.fixture('tasks/get').then(function (tasks) {
+            this.tasks = tasks
+        })
+    })
 
     it('get my tasks', function () {
 
-    })
+        const { user, tasks } = this.tasks.list
 
+        cy.task('deleteTasksLike', 'Estud4r')
+
+        cy.task('deleteUser', user.email)
+        cy.postUser(user)
+
+        cy.postSession(user)
+            .then(respUser => {
+
+                tasks.forEach(function (t) {
+                    cy.postTask(t, respUser.body.token)
+                })
+
+                cy.api({
+                    url: '/tasks',
+                    method: 'GET',
+                    headers: {
+                        authorization: respUser.body.token
+                    },
+                    failOnStatusCode: false
+                }).then(response => {
+                    expect(response.status).to.eq(200)
+                }).its('body')
+                    .should('be.an', 'array')
+                    .and('have.length', tasks.length)
+            })
+    })
 })
